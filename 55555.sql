d5725a8fd606bd1312c97af6c98497227f5e79bd
xl881221 <648379297@qq.com>
2016-12-13 11:43:54 +0800

    jjjhgdfhf

---
+++ 55555.sql
@@ -0,0 +1,71 @@
+prompt Importing table u_base_inst...
+set feedback off
+set define off
+
+insert into u_base_inst (INST_ID, INST_NAME, INST_SMP_NAME, PARENT_INST_ID, INST_LAYER, ADDRESS, ZIP, TEL, FAX, IS_BUSSINESS, ORDER_NUM, DESCRIPTION, START_DATE, END_DATE, CREATE_TIME, ENABLED, INST_REGION, EMAIL, INST_PATH, INST_LEVEL, IS_HEAD, TAXPERNAME, TAXPERNUMBER, ACCOUNT, TAXADDRESS, TAXTEL, TAXBANK, TAXPAYER_TYPE)
+values ('863203', '光大永明人寿保险有限公司江苏分公司无锡中心支公司 
+', '无锡中心支公司 
+', '863200', 2, null, null, null, null, 'true', 0, null, to_date('28-04-2016 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), to_date('21-01-2065 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), to_date('28-04-2016 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), 'true', null, null, '\86\863200\863203\', 3, 'false', '光大永明人寿保险有限公司江苏分公司无锡中心支公司 
+', '91320200670118606M
+', '39920188000079663
+', '无锡市滨湖区金融一街10号无锡金融中心17层01单元
+', '025-66671188
+', '中国光大银行股份有限公司无锡分行营业部
+', '0');
+
+insert into u_base_inst (INST_ID, INST_NAME, INST_SMP_NAME, PARENT_INST_ID, INST_LAYER, ADDRESS, ZIP, TEL, FAX, IS_BUSSINESS, ORDER_NUM, DESCRIPTION, START_DATE, END_DATE, CREATE_TIME, ENABLED, INST_REGION, EMAIL, INST_PATH, INST_LEVEL, IS_HEAD, TAXPERNAME, TAXPERNUMBER, ACCOUNT, TAXADDRESS, TAXTEL, TAXBANK, TAXPAYER_TYPE)
+values ('863204', '光大永明人寿保险有限公司江苏分公司扬州中心支公司
+', '扬州中心支公司
+', '863200', 2, null, null, null, null, 'true', 0, null, to_date('28-04-2016 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), to_date('21-01-2065 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), to_date('28-04-2016 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), 'true', null, null, '\86\863200\863204\', 3, 'false', '光大永明人寿保险有限公司江苏分公司扬州中心支公司
+', '9132100067204142X4
+', '1108020909100110682
+', '扬州市邗江区文汇西路268号1301室
+', '025-66671188
+', '中国工商银行股份有限公司扬州汶河支行
+', '0');
+
+insert into u_base_inst (INST_ID, INST_NAME, INST_SMP_NAME, PARENT_INST_ID, INST_LAYER, ADDRESS, ZIP, TEL, FAX, IS_BUSSINESS, ORDER_NUM, DESCRIPTION, START_DATE, END_DATE, CREATE_TIME, ENABLED, INST_REGION, EMAIL, INST_PATH, INST_LEVEL, IS_HEAD, TAXPERNAME, TAXPERNUMBER, ACCOUNT, TAXADDRESS, TAXTEL, TAXBANK, TAXPAYER_TYPE)
+values ('863205', '光大永明人寿保险有限公司江苏分公司常州中心支公司
+', '常州中心支公司
+', '863200', 2, null, null, null, null, 'true', 0, null, to_date('28-04-2016 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), to_date('21-01-2065 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), to_date('28-04-2016 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), 'true', null, null, '\86\863200\863205\', 3, 'false', '光大永明人寿保险有限公司江苏分公司常州中心支公司
+', '91320400567808051U
+', '76610188000036776
+', '常州市新北区万达广场C座2-2404，2405室
+', '025-66671188
+', '中国光大银行常州支行
+', '0');
+
+insert into u_base_inst (INST_ID, INST_NAME, INST_SMP_NAME, PARENT_INST_ID, INST_LAYER, ADDRESS, ZIP, TEL, FAX, IS_BUSSINESS, ORDER_NUM, DESCRIPTION, START_DATE, END_DATE, CREATE_TIME, ENABLED, INST_REGION, EMAIL, INST_PATH, INST_LEVEL, IS_HEAD, TAXPERNAME, TAXPERNUMBER, ACCOUNT, TAXADDRESS, TAXTEL, TAXBANK, TAXPAYER_TYPE)
+values ('863206', '光大永明人寿保险有限公司江苏分公司徐州中心支公司
+', '徐州中心支公司
+', '863200', 2, null, null, null, null, 'true', 0, null, to_date('28-04-2016 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), to_date('21-01-2065 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), to_date('28-04-2016 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), 'true', null, null, '\86\863200\863206\', 3, 'false', '光大永明人寿保险有限公司江苏分公司徐州中心支公司
+', '913203005643228731
+', '1106020209210181878
+', '徐州市建国东路111号广达大厦11楼
+', '025-66671188
+', '中国工商银行徐州市云龙支行
+', '0');
+
+insert into u_base_inst (INST_ID, INST_NAME, INST_SMP_NAME, PARENT_INST_ID, INST_LAYER, ADDRESS, ZIP, TEL, FAX, IS_BUSSINESS, ORDER_NUM, DESCRIPTION, START_DATE, END_DATE, CREATE_TIME, ENABLED, INST_REGION, EMAIL, INST_PATH, INST_LEVEL, IS_HEAD, TAXPERNAME, TAXPERNUMBER, ACCOUNT, TAXADDRESS, TAXTEL, TAXBANK, TAXPAYER_TYPE)
+values ('863207', '光大永明人寿保险有限公司泰州中心支公司
+', '泰州中心支公司
+', '863200', 2, null, null, null, null, 'true', 0, null, to_date('28-04-2016 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), to_date('21-01-2065 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), to_date('28-04-2016 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), 'true', null, null, '\86\863200\863207\', 3, 'false', '光大永明人寿保险有限公司泰州中心支公司
+', '91321200576684249Q
+', '1115061309000023501
+', '泰州市海陵区江洲南路8号2楼
+', '025-66671188
+', '中国工商银行泰州大林桥支行
+', '0');
+
+insert into u_base_inst (INST_ID, INST_NAME, INST_SMP_NAME, PARENT_INST_ID, INST_LAYER, ADDRESS, ZIP, TEL, FAX, IS_BUSSINESS, ORDER_NUM, DESCRIPTION, START_DATE, END_DATE, CREATE_TIME, ENABLED, INST_REGION, EMAIL, INST_PATH, INST_LEVEL, IS_HEAD, TAXPERNAME, TAXPERNUMBER, ACCOUNT, TAXADDRESS, TAXTEL, TAXBANK, TAXPAYER_TYPE)
+values ('863208', '光大永明人寿保险有限公司江苏分公司盐城中心支公司
+', '盐城中心支公司
+', '863200', 2, null, null, null, null, 'true', 0, null, to_date('28-04-2016 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), to_date('21-01-2065 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), to_date('28-04-2016 02:53:40', 'dd-mm-yyyy hh24:mi:ss'), 'true', null, null, '\86\863200\863208\', 3, 'false', '光大永明人寿保险有限公司江苏分公司盐城中心支公司
+', '913209025810132040
+', '32001738636052508950
+', '盐城市青年路6号水岸名都丁香园1号楼402室
+', '025-66671188
+', '中国建设银行股份有限公司盐城市分行营业部
+', '0');
+
+prompt Done.
